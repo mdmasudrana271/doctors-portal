83de6bb3d8a2234cc039475296234b16bc09493d
md masud rana <mr7123619@gmail.com>
2022-11-12 12:29:29 +0600

    doctors portal setup project and create home page banner and info cards

---
+++ src/pages/Home/Services/Services.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <section className='mt-32'>
+            <div className='text-center'>
+                <h4 className='text-primary text-xl font-bold'>OUR SERVICES</h4>
+                <h2 className='text-4xl mt-2'>Services We Provide</h2>
+            </div>
+            <div></div>
+        </section>
+    );
+};
+
+export default Services;
